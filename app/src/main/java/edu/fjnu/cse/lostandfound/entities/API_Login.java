0d1704d1d77a3b84fc678e891cdf707054680320
Jam <704017396@qq.com>
2017-06-20 01:04:33 +0800

    edit API_Login

--- app/src/main/java/edu/fjnu/cse/lostandfound/entities/API_Login.java
+++ app/src/main/java/edu/fjnu/cse/lostandfound/entities/API_Login.java
@@ -18,5 +18,35 @@ public class API_Login {
         this.captcha = captcha;
     }
 
+    public boolean isHasCaptcha() {
+        return hasCaptcha;
+    }
 
+    public void setHasCaptcha(boolean hasCaptcha) {
+        this.hasCaptcha = hasCaptcha;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCaptcha() {
+        return captcha;
+    }
+
+    public void setCaptcha(String captcha) {
+        this.captcha = captcha;
+    }
 }
