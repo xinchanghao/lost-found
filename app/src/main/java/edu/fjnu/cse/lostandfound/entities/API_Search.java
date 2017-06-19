bc6ffaa8147a9e0a51fa720d4e53551d74e0e95e
Jam <704017396@qq.com>
2017-06-20 01:06:25 +0800

    edit API_Search

--- app/src/main/java/edu/fjnu/cse/lostandfound/entities/API_Search.java
+++ app/src/main/java/edu/fjnu/cse/lostandfound/entities/API_Search.java
@@ -9,23 +9,7 @@ public class API_Search {
     private String text;
     private int page;
 
-    public API_Search(int page, String text) {
-        this.text = text;
-        this.page = page;
-    }
 
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public String getText() {
-
-        return text;
-    }
 
     public void setText(String text) {
         this.text = text;
